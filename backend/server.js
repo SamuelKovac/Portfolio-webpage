0e5f16ecbdf6e776be61b757fe2ab792e827a24d
Kango <2810379@ucm.sk>
2026-06-18 00:34:16 +0200

    Backend inicialization, Express and nodemon installation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-��
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/api/status', (req, res) => {
+    res.json({ message: "Server beží úspešne!" });
+});
+
+app.get('/api/about', (req, res) => {
+    res.json({
+        name: "Samuel z HNP",
+        role: "Fullstack Developer"
+    });
+});
+
+app.listen(PORT, () => {console.log('Server načítaný...');
+});
