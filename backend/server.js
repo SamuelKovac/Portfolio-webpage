5cccfa39c38c867b3dcdd5a4fa05c067bebe0cd5
Kango <2810379@ucm.sk>
2026-06-23 23:45:32 +0200

    Connecting frontend with backend using fetch, CORS installation

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.get('/api/status', (req, res) => {
     res.json({ message: "Server beží úspešne!" });
 });
